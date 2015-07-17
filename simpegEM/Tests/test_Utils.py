0b6607065339fc46addb5b1fc6535982b3cad6e3
Lindsey Heagy <lheagy@eos.ubc.ca>
2015-07-17 09:19:11 -0700

    rotation from one normal to another + test

--- simpegEM/Tests/test_Utils.py
+++ simpegEM/Tests/test_Utils.py
@@ -1,18 +1,31 @@
 import unittest, os
 import numpy as np
+from SimPEG import Utils
 from simpegEM.Utils import coordUtils
 
-tol = 1e-20
+tol = 1e-15
 
 class coorUtilsTest(unittest.TestCase):
 
     def test_CrossProd(self):
-        self.assertTrue(all(coordUtils.crossProd(np.r_[1.,0.,0.],np.r_[0.,1.,0.]) - np.r_[0.,0.,1.]) < tol)
-        self.assertTrue(all(coordUtils.crossProd(np.r_[np.sqrt(2.),np.sqrt(2.),0.],np.r_[np.sqrt(2.),np.sqrt(2.),0.]) - np.r_[0.,0.,1.]) < tol)
-        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,1.,0.],np.r_[0.,0.,1.]) - np.r_[1.,0.,0.]) < tol)
-        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,np.sqrt(2.),np.sqrt(2.)],np.r_[0.,np.sqrt(2.),np.sqrt(2.)]) - np.r_[1.,0.,0.]) < tol)
-        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,0.,1.],np.r_[1.,0.,0.]) - np.r_[0.,1.,0.]) < tol)
-        self.assertTrue(all(coordUtils.crossProd(np.r_[np.sqrt(2.),0.,np.sqrt(2.)],np.r_[np.sqrt(2.),0.,np.sqrt(2.)]) - np.r_[0.,1.,0.]) < tol)
+        self.assertTrue(all(coordUtils.crossProd(np.r_[1.,0.,0.],np.r_[0.,1.,0.]) - np.r_[0.,0.,1.] < tol))
+        self.assertTrue(all(coordUtils.crossProd(np.r_[np.sqrt(2.),np.sqrt(2.),0.],np.r_[np.sqrt(2.),np.sqrt(2.),0.]) - np.r_[0.,0.,1.] < tol))
+        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,1.,0.],np.r_[0.,0.,1.]) - np.r_[1.,0.,0.] < tol))
+        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,np.sqrt(2.),np.sqrt(2.)],np.r_[0.,np.sqrt(2.),np.sqrt(2.)]) - np.r_[1.,0.,0.] < tol))
+        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,0.,1.],np.r_[1.,0.,0.]) - np.r_[0.,1.,0.] < tol))
+        self.assertTrue(all(coordUtils.crossProd(np.r_[np.sqrt(2.),0.,np.sqrt(2.)],np.r_[np.sqrt(2.),0.,np.sqrt(2.)]) - np.r_[0.,1.,0.] < tol))
+
+    def test_RotMat(self):
+        v0 = np.random.rand(3)
+        v0 *= 1./np.linalg.norm(v0)
+        v1 = np.random.rand(3)
+        v1 *= 1./np.linalg.norm(v1)
+        Rf = coordUtils.rotationMatFromNormals(v0,v1)
+        Ri = coordUtils.rotationMatFromNormals(v1,v0)
+
+        self.assertTrue(all(Utils.mkvc(Rf.dot(v0) - v1) < tol))
+        self.assertTrue(all(Utils.mkvc(Ri.dot(v1) - v0) < tol))
+        
 
 if __name__ == '__main__':
     unittest.main()
--- simpegEM/Utils/coordUtils.py
+++ simpegEM/Utils/coordUtils.py
@@ -8,7 +8,7 @@ def crossProd(v0,v1):
     assert len(v0) == 3, "Length of v0 should be 3"
     assert len(v1) == 3, "Length of v1 should be 3"
 
-    v2 = np.zeros(3)
+    v2 = np.zeros(3,dtype=float)
 
     v2[0] = v0[1]*v1[2] - v1[1]*v0[2]
     v2[1] = v1[0]*v0[2] - v0[0]*v1[2]
@@ -16,18 +16,34 @@ def crossProd(v0,v1):
 
     return v2
 
-def rotateFromNormals(n0,n1):
-	"""
-		Performs the minimum number of rotations to define a rotation from the direction indicated by the vector n0 to the direction indicated by n1.
-		The axis of rotation is n0 x n1
+def rotationMatFromNormals(n0,n1):
+    """
+    	Performs the minimum number of rotations to define a rotation from the direction indicated by the vector n0 to the direction indicated by n1.
+    	The axis of rotation is n0 x n1
+        https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
 
-		Follows: http://graphics.cs.ucdavis.edu/~joy/ecs178/Transformations/General-Rotation.pdf
-	"""
+    """
 
-	# ensure both n0, n1 are vectors of length 1
-	assert len(n0) == 3, 'Length of n0 should be 3'
-	assert len(n1) == 3, 'Length of n1 should be 3'
+    # ensure both n0, n1 are vectors of length 1
+    assert len(n0) == 3, "Length of n0 should be 3"
+    assert len(n1) == 3, "Length of n1 should be 3"
 
-	# ensure both are true normals
-	n0 = n0*1./np.linalg.norm(n0)
-	n1 = n1*1./np.linalg.norm(n1)
+    # ensure both are true normals
+    n0 *= 1./np.linalg.norm(n0)
+    n1 *= 1./np.linalg.norm(n1)
+
+    n0dotn1 = n0.dot(n1) 
+
+    # define the rotation axis, which is the cross product of the two vectors
+    rotAx = crossProd(n0,n1)
+    rotAx *= 1./np.linalg.norm(rotAx)
+
+    cosT = n0dotn1/(np.linalg.norm(n0)*np.linalg.norm(n1))
+    sinT = np.sqrt(1.-n0dotn1**2)
+
+    ux = np.array([[0., -rotAx[2], rotAx[1]], [rotAx[2], 0., -rotAx[0]], [-rotAx[1], rotAx[0], 0.]],dtype=float)
+
+    return np.eye(3,dtype=float) + sinT*ux + (1.-cosT)*(ux.dot(ux))
+
+
+
