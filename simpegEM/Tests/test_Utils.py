5982c8c2b8163bb03b3a479f437f2bcaddc376f0
Lindsey <lindseyheagy@gmail.com>
2015-07-28 13:35:24 -0700

    rotate from normals

--- simpegEM/Tests/test_Utils.py
+++ simpegEM/Tests/test_Utils.py
@@ -7,25 +7,29 @@ tol = 1e-15
 
 class coorUtilsTest(unittest.TestCase):
 
-    def test_CrossProd(self):
-        self.assertTrue(all(coordUtils.crossProd(np.r_[1.,0.,0.],np.r_[0.,1.,0.]) - np.r_[0.,0.,1.] < tol))
-        self.assertTrue(all(coordUtils.crossProd(np.r_[np.sqrt(2.),np.sqrt(2.),0.],np.r_[np.sqrt(2.),np.sqrt(2.),0.]) - np.r_[0.,0.,1.] < tol))
-        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,1.,0.],np.r_[0.,0.,1.]) - np.r_[1.,0.,0.] < tol))
-        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,np.sqrt(2.),np.sqrt(2.)],np.r_[0.,np.sqrt(2.),np.sqrt(2.)]) - np.r_[1.,0.,0.] < tol))
-        self.assertTrue(all(coordUtils.crossProd(np.r_[0.,0.,1.],np.r_[1.,0.,0.]) - np.r_[0.,1.,0.] < tol))
-        self.assertTrue(all(coordUtils.crossProd(np.r_[np.sqrt(2.),0.,np.sqrt(2.)],np.r_[np.sqrt(2.),0.,np.sqrt(2.)]) - np.r_[0.,1.,0.] < tol))
+    def test_crossProd(self):
+        self.assertTrue(np.linalg.norm(coordUtils.crossProd(np.r_[1.,0.,0.],np.r_[0.,1.,0.]) - np.r_[0.,0.,1.]) < tol)
+        self.assertTrue(np.linalg.norm(coordUtils.crossProd(np.r_[0.,1.,0.],np.r_[0.,0.,1.]) - np.r_[1.,0.,0.]) < tol)
+        self.assertTrue(np.linalg.norm(coordUtils.crossProd(np.r_[0.,0.,1.],np.r_[1.,0.,0.]) - np.r_[0.,1.,0.]) < tol)
 
-    def test_RotMat(self):
+    def test_rotationMatrixFromNormals(self):
         v0 = np.random.rand(3)
         v0 *= 1./np.linalg.norm(v0)
         v1 = np.random.rand(3)
         v1 *= 1./np.linalg.norm(v1)
-        Rf = coordUtils.rotationMatFromNormals(v0,v1)
-        Ri = coordUtils.rotationMatFromNormals(v1,v0)
+        Rf = coordUtils.rotationMatrixFromNormals(v0,v1)
+        Ri = coordUtils.rotationMatrixFromNormals(v1,v0)
 
-        self.assertTrue(all(Utils.mkvc(Rf.dot(v0) - v1) < tol))
-        self.assertTrue(all(Utils.mkvc(Ri.dot(v1) - v0) < tol))
-        
+        self.assertTrue(np.linalg.norm(Utils.mkvc(Rf.dot(v0) - v1)) < tol)
+        self.assertTrue(np.linalg.norm(Utils.mkvc(Ri.dot(v1) - v0)) < tol)
+
+    def test_rotatePointsFromNormals(self):
+        v0 = np.random.rand(3)
+        v0*= 1./np.linalg.norm(v0)
+        v1 = np.random.rand(3) 
+        v1*= 1./np.linalg.norm(v1)   
+
+        self.assertTrue(np.linalg.norm(Utils.mkvc(coordUtils.rotatePointsFromNormals(Utils.mkvc(v0,2).T,v0,v1))-v1) < tol)
 
 if __name__ == '__main__':
     unittest.main()
--- simpegEM/Utils/coordUtils.py
+++ simpegEM/Utils/coordUtils.py
@@ -3,6 +3,11 @@ import numpy as np
 def crossProd(v0,v1):
     """
     	Cross product of 2 vectors
+
+        :param numpy.array v0: vector of length 3
+        :param numpy.array v1: vector of length 3
+        :rtype: numpy.array
+        :return: cross product of v0,v1
     """ 
     # ensure both n0, n1 are vectors of length 1
     assert len(v0) == 3, "Length of v0 should be 3"
@@ -16,21 +21,26 @@ def crossProd(v0,v1):
 
     return v2
 
-def rotationMatFromNormals(n0,n1):
+def rotationMatrixFromNormals(v0,v1):
     """
     	Performs the minimum number of rotations to define a rotation from the direction indicated by the vector n0 to the direction indicated by n1.
     	The axis of rotation is n0 x n1
         https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
 
+        :param numpy.array v0: vector of length 3
+        :param numpy.array v1: vector of length 3
+        :rtype: numpy.array, 3x3
+        :return: rotation matrix which rotates the frame so that n0 is aligned with n1
+
     """
 
     # ensure both n0, n1 are vectors of length 1
-    assert len(n0) == 3, "Length of n0 should be 3"
-    assert len(n1) == 3, "Length of n1 should be 3"
+    assert len(v0) == 3, "Length of n0 should be 3"
+    assert len(v1) == 3, "Length of n1 should be 3"
 
     # ensure both are true normals
-    n0 *= 1./np.linalg.norm(n0)
-    n1 *= 1./np.linalg.norm(n1)
+    n0 = v0*1./np.linalg.norm(v0)
+    n1 = v1*1./np.linalg.norm(v1)
 
     n0dotn1 = n0.dot(n1) 
 
@@ -44,6 +54,23 @@ def rotationMatFromNormals(n0,n1):
     ux = np.array([[0., -rotAx[2], rotAx[1]], [rotAx[2], 0., -rotAx[0]], [-rotAx[1], rotAx[0], 0.]],dtype=float)
 
     return np.eye(3,dtype=float) + sinT*ux + (1.-cosT)*(ux.dot(ux))
+
+
+def rotatePointsFromNormals(XYZ,n0,n1): #,origin=np.r_[0.,0.,0.])
+    """
+        rotates a grid so that the vector n0 is aligned with the vector n1
+
+        :param numpy.array n0: vector of length 3, should have norm 1
+        :param numpy.array n1: vector of length 3, should have norm `
+        :rtype: numpy.array, 3x3
+        :return: rotation matrix which rotates the frame so that n0 is aligned with n1
+    """
+
+    R = rotationMatrixFromNormals(n0, n1)
+
+    assert XYZ.shape[1] == 3, "Grid XYZ should be 3 wide"
+    return XYZ.dot(R.T)
+
 
 
 
